ac75f3a22ff0fabb062ad34b303e4987c6ec10b6
hoangtrungchinh <chinhguitar@gmail.com>
2020-05-16 17:59:42 +0700

    refactor script init

--- init.py
+++ init.py
@@ -1,20 +1,27 @@
 # Clean current migrations
 import os
+import subprocess
 
 os.system('python3 manage.py migrate --fake cat zero')
 os.system('find . -path "*/migrations/*.py" -not -name "__init__.py" -delete')
 os.system('find . -path "*/migrations/*.pyc"  -delete ')
 
-# Remove and create DB in Postgres
+print("\n====== Remove and create DB in Postgres ====== ")
 # https://pypi.org/project/django-postgres-dropdb/
 # https://pypi.org/project/django-postgres-createdb/
 os.system('python3 manage.py dropdb')
 os.system('python3 manage.py createdb')
 
-# migrate DB
+print("\n====== migrate DB ====== ")
 os.system('python3 manage.py makemigrations')
 os.system('python3 manage.py migrate')
 
-# Load init Data
+
+print("\n====== Load init Data ====== ")
 os.system('python3 manage.py loaddata initial_1k_data.yaml')
-os.system('python3 manage.py search_index --rebuild')
+
+
+print("\n====== Rebuild in Elastic Search ====== ")
+subprocess.Popen(['echo Y | python3 manage.py search_index --rebuild'], shell=True)
+
+print("\n====== Proceess Complete ====== ")
