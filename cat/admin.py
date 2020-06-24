43851d310373e2ed6d87417e36aa55a899c47dc4
hoangtrungchinh <chinhguitar@gmail.com>
2020-06-24 16:45:04 +0700

    add column id to admin page

--- cat/admin.py
+++ cat/admin.py
@@ -14,35 +14,35 @@ from .models import (
 
 
 class TranslationMemoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "src_lang", "tar_lang", "user")
+    list_display = ("id", "name", "description", "src_lang", "tar_lang", "user")
 
 
 class TMContentAdmin(admin.ModelAdmin):
-    list_display = ("src_sentence", "tar_sentence", "translation_memory")
+    list_display = ("id", "src_sentence", "tar_sentence", "translation_memory")
     
 
 class GlossaryTypeAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "user")
+    list_display = ("id", "name", "description", "user")
 
 
 class GlossaryAdmin(admin.ModelAdmin):
-    list_display = ("description", "name", "src_lang", "tar_lang", "user", "get_gloss_types")
+    list_display = ("id", "description", "name", "src_lang", "tar_lang", "user", "get_gloss_types")
 
 
 class GlossaryContentAdmin(admin.ModelAdmin):
-    list_display = ("src_phrase", "tar_phrase", "glossary")
+    list_display = ("id", "src_phrase", "tar_phrase", "glossary")
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("name", "user", "src_lang", "tar_lang", "translate_service", "list_glossary", "list_translation_memory")
+    list_display = ("id", "name", "user", "src_lang", "tar_lang", "translate_service", "list_glossary", "list_translation_memory")
 
 
 class FileAdmin(admin.ModelAdmin):
-    list_display = ("file", "project", "confirm",)
+    list_display = ("id", "file", "project", "confirm",)
 
 
 class SentenceAdmin(admin.ModelAdmin):
-    list_display = ("file", "src_str", "tar_str", "score", "is_confirmed", "tag")
+    list_display = ("id", "file", "src_str", "tar_str", "score", "is_confirmed", "tag")
 
 
 
